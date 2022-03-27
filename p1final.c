b69607fa9ac1a389bcd9cddba3c0e611c0c8fcda
Sandhya Reddy <94678086+Sandhyareddychinni@users.noreply.github.com>
2022-03-27 10:00:23 +0000

    p1

--- p1final.c
+++ p1final.c
@@ -2,16 +2,16 @@
 void input(int *a,int *b)
 {
   printf("enter the numbers/n");
-  scanf("%d %d",a,b);
-}
+scanf("%d %d",a,b);
+  }
 void add(int a,int b,int *sum)
 {
   *sum=a+b;
-}
+  }
 void output(int a,int b,int sum)
 {
   printf("sum of %d+%d is %d\n",a,b,sum);
-}
+  }
 int main()
 {
   int a,b,sum;
@@ -19,4 +19,4 @@ int main()
   add(a,b,&sum);
   output(a,b,sum);
   return 0;
-}
+  }
