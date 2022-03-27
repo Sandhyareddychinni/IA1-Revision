4ff2dfdc6e23b41fa660f6d319d8601698b2547b
Sandhya Reddy <94678086+Sandhyareddychinni@users.noreply.github.com>
2022-03-27 10:29:23 +0000

    p3

--- p3final.c
+++ p3final.c
@@ -5,23 +5,23 @@ int input_n()
   printf("enter the number:\n");
   scanf("%d",&n);
   return n;
-}
+  }
 int sum_n(int n)
 {
   int i,sum=0;
   for(i=1; i<=n; i++)
-{
-  sum=sum+i;
-}
-return sum;
-}
+    {
+      sum=sum+i;
+      }
+  return sum;
+  }
 void output(int sum,int n)
 {
   printf("sum=%d",sum);
-}
+  }
 int main()
 {
-  int n,sum,i;
+int n,sum,i;
   n=input_n();
   sum=sum_n(n);
   output(sum,n);
